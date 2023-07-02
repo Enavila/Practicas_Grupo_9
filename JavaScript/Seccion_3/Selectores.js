5f7b26bdef731c294229ed21dbf5373db0eee3fb
Haonter <101908524+Haonter@users.noreply.github.com>
2023-07-02 11:00:10 -0400

    Selectores

--- JavaScript/Seccion_3/Selectores.js
+++ JavaScript/Seccion_3/Selectores.js
@@ -37,17 +37,31 @@
 
 let miTitulo = document.getElementById("titulo");
 console.log(miTitulo);
-console.log(miTitulo.innerText);
-
-
 
 
 
 // Seleccion de multiples elementos mediante tagName
 let parrafos = document.getElementsByTagName("p");
-console.log(parrafos[0].innerText);
-console.log(parrafos[1].innerText);
-console.log(parrafos[2].innerText);
-console.log(parrafos[3].innerText);
-console.log(parrafos[4].innerText);
-console.log(parrafos[5].innerText);
+console.log("Lista de parrafos seleccionados por nombre de etiqueta");
+console.log(parrafos);
+
+// Mostrar especificamente el elemento numero 2
+console.log();
+
+
+// Seleccion de multiples elementos mediante Name
+let mis_spans = document.getElementsByName("miSpan");
+console.log("Lista de spans seleccionados por propiedad nombre");
+console.log(mis_spans);
+
+// Mostrar especificamente el elemento numero 4
+console.log();
+
+
+// Seleccion de multiples elementos mediante ClassName
+let misDivs = document.getElementsByClassName("miClase");
+console.log("Lista de divs seleccionados por nombre de clase");
+console.log(misDivs);
+
+// Mostrar especificamente el elemento numero 6
+console.log();
